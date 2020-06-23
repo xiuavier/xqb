61a6aa7372128d0b5663c1dfaf69cbad89b5c678
xavier <1014334773@qq.com>
2020-06-23 10:46:46 +0800

    修改控制器路径，第3次提交

--- app/Http/MiniApi/Controller/IndexController.php
+++ app/Http/MiniApi/Controller/IndexController.php
@@ -4,17 +4,8 @@
 namespace App\Http\MiniApi\Controller;
 
 
-use App\Exception\ApiException;
-use App\Http\Middleware\TokenMiddleware;
-use App\Http\MiniApi\Common\ReturnMessage;
-use App\Http\MiniApi\Service\LoginService;
-use Swoft\Bean\Annotation\Mapping\Inject;
-use Swoft\Db\Exception\DbException;
-use Swoft\Http\Message\Request;
 use Swoft\Http\Server\Annotation\Mapping\Controller;
-use Swoft\Http\Server\Annotation\Mapping\Middleware;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
-use Swoft\Validator\Annotation\Mapping\Validate;
 
 /**
  * Class IndexController
